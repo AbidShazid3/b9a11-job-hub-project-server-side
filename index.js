990d40aed5f449d84d34eb694f4c927d2d62e16b
AbidShazid3 <abidshazid3@gmail.com>
2024-05-11 05:24:22 +0600

    added all job data fetching

--- index.js
+++ index.js
@@ -6,7 +6,7 @@ const app = express();
 const port = process.env.PORT || 5000;
 
 // middleware
-app.use(cors());
+app.use(cors({ origin: ['http://localhost:5173'], credentials: true }));
 app.use(express.json());
 
 
@@ -28,6 +28,11 @@ async function run() {
         await client.connect();
 
         const jobsCollection = client.db('jobDB').collection('jobs');
+
+        app.get('/jobs', async (req, res) => {
+            const result = await jobsCollection.find().toArray();
+            res.send(result);
+        })
 
         app.post('/jobs', async (req, res) => {
             const job = req.body;
